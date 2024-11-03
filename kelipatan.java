bbf1f699687348064932e026507b3266b0816dd0
intannasr28 <155829473+intannasr28@users.noreply.github.com>
2024-11-03 17:15:50 +0700

    Add files via upload

---
+++ kelipatan.java
@@ -0,0 +1,14 @@
+package pert6;
+public class kelipatan {
+    public static void main(String[] args) {
+         double angka = 3.5;
+        System.out.println("Bilangan kelipatan 3 dan 5 dari 3 sampai 35:");
+
+         while (angka <= 35) {
+            System.out.println(angka);
+            angka += 3.5;
+                
+            }
+        }
+    }
+
